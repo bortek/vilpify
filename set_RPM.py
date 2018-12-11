ca95c2a995835da58f92724a3fa0ee3c2dd3a9f1
root <bortek666@gmail.com>
2018-12-11 21:10:29 +0100

    changing invert stuff

--- set_RPM.py
+++ set_RPM.py
@@ -9,7 +9,7 @@ import pigpio
 freq=5000
 GPIO=18
 # pwm example , if you want 10% then set pwm=100000, 90%=900000
-pwm=300000
+pwm=900000
 
 # TODO
 # Limit max PWM to 90% to save the motor
@@ -21,10 +21,11 @@ if not pi.connected:
 # For some reason the signal from the PCB module is inverted
 # therefore we need to invert PWM signal like inv_pwm=100-pwm
 
-# invert pwm here
-inv_pwm = 1000000 - pwm
-pi.hardware_PWM(GPIO, freq, inv_pwm) # 500000 for square wave.
-#print("\nSetting PWM to {e} got {} Setting PWM to {}%".format(freq, pi.get_PWM_frequency(GPIO),inv_pwm/10000))
+# enable this if you want to invert pwm
+#set_pwm = 1000000 - pwm
+set_pwm = pwm 
+pi.hardware_PWM(GPIO, freq, set_pwm) # 500000 for square wave.
+#print("\nSetting PWM to {e} got {} Setting PWM to {}%".format(freq, pi.get_PWM_frequency(GPIO),set_pwm/10000))
 time.sleep(3)
 
 # Set rpm to 0 by sending PWM 0
--- test2.py
+++ test2.py
@@ -15,17 +15,18 @@ if not pi.connected:
 # therefore we need to invert PWM signal like inv_pwm=100-pwm
 
 min_pwm = 000000
-max_pwm = 1100000
+max_pwm = 11000000
 step_pwm = 100000
 
 for pwm in range(min_pwm, max_pwm, step_pwm):
-   # invert pwm here
-   inv_pwm = 1000000 - pwm
-   pi.hardware_PWM(18, freq, inv_pwm) # 500000 for square wave.
-   print("\n12 set {} got {} inv_pwm={}%".format(
-     freq, pi.get_PWM_frequency(18),inv_pwm/10000))
-   time.sleep(5)
+   # enable this if you want to invert pwm
+   #set_pwm = 1000000 - pwm
+   set_pwm = pwm
+   pi.hardware_PWM(18, freq, set_pwm) # 500000 for square wave.
+   print("\n12 set freq {} got freq {} set_pwm={}%".format(freq, pi.get_PWM_frequency(18),set_pwm/10000))
+   time.sleep(2)
 
-pi.hardware_PWM(18, 0, 0)
+# uncomment this if you want to set 0 PWM when above loop is complete
+#pi.hardware_PWM(18, 0, 0)
 
 pi.stop()
