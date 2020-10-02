71019a06ab42583f47560a971f6bb327b9c04b9e
root <bortek666@gmail.com>
2020-10-02 22:30:40 +0200

    adding args parsing

--- set_RPM.py
+++ set_RPM.py
@@ -5,25 +5,43 @@
 
 import time
 import pigpio
+import argparse
 
 freq=5000
 GPIO=18
-# pwm example , if you want 10% then set pwm=100000, 90%=900000
-pwm=300000
+min_percent=0
+max_percent=80
 
-# TODO
-# Limit max PWM to 90% to save the motor
+# pwm example in case of hard coding them. If you want 10% then set pwm=100000, 90%=900000
+#pwm=200000
+#pwm=400000
+#pwm=350000
 
+# parsing arguments
+# Limit max PWM to 80% to spare the motor
+def range_type(astr, min=min_percent, max=max_percent):
+    value = int(astr)
+    if min<= value <= max:
+        return value
+    else:
+        raise argparse.ArgumentTypeError('value not in range %s-%s'%(min,max))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--percent', dest='speed_percent', type=range_type,
+                    help='Set fan speed to INTEGER percent. Values in range %s-%s are accepted.'%(min_percent,max_percent), metavar="[%s-%s]"%(min_percent,max_percent))
+args = parser.parse_args()
+
+# Setting PWM
 pi = pigpio.pi()
 
 if not pi.connected:
    exit()
-# For some reason the signal from the PCB module is inverted
-# therefore we need to invert PWM signal like inv_pwm=100-pwm
 
-# enable this if you want to invert pwm
-#set_pwm = 1000000 - pwm
-set_pwm = pwm 
+# If you want to hard code pwm (taken from beginnig
+# of file) then enable this and disable setting it from parsed arguments
+#set_pwm = pwm 
+set_pwm = args.speed_percent * 10000
+
 pi.hardware_PWM(GPIO, freq, set_pwm) # 500000 for square wave.
 #print("\nSetting PWM to {e} got {} Setting PWM to {}%".format(freq, pi.get_PWM_frequency(GPIO),set_pwm/10000))
 time.sleep(3)
